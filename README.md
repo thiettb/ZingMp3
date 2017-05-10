610df1f961389753cab675f99d520c57d9a77044
Macbook <macbook@MacBook-Pro-cua-Macbook.local>
2017-05-11 00:55:43 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+iZingMp3 written by Tran Ba Thiet
+
+for iOS 10.0
+
+Language: Swift 3.0
+
+MIT license
--- Zing Mp3/ViewController.swift
+++ Zing Mp3/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lbl_zingmp3: UILabel!
 
+    @IBOutlet weak var img_ZingMp3: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        lbl_zingmp3!.alpha = 0
+        img_ZingMp3!.alpha = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +23,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIView.animate(withDuration: 4, animations: {
+           self.img_ZingMp3.alpha = 1
+        }){ (finished) in UIView.animate(withDuration: 3, animations: {
+            self.lbl_zingmp3.center = CGPoint(x: self.img_ZingMp3.center.x, y: 100)
+            self.lbl_zingmp3.alpha = 1
+        })
+        
+        }
+            
+        }
 
 
 }
